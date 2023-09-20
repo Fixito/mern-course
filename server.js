586e45e0e3a6413bfa4accb4a42ed654ff19d309
Thomas ROBERT <thomas_3004@hotmail.fr>
2023-09-20 09:40:41 +0200

    Add trust proxy

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+app.set('trust proxy', 1);
 app.use(express.static(path.resolve(__dirname, './client/dist')));
 app.use(cookieParser());
 app.use(express.json());
